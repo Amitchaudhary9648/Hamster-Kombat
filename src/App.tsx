59bae2466420f12b33792ff752730bb8d753e627
Amit Chaudhary <amit@ith.tech>
2024-07-08 11:53:29 +0530

    remove unused code

--- src/App.tsx
+++ src/App.tsx
@@ -46,24 +46,24 @@ const App: React.FC = () => {
   // const [dailyCipherTimeLeft, setDailyCipherTimeLeft] = useState("");
   // const [dailyComboTimeLeft, setDailyComboTimeLeft] = useState("");
 
-  const calculateTimeLeft = (targetHour: number) => {
-    const now = new Date();
-    const target = new Date(now);
-    target.setUTCHours(targetHour, 0, 0, 0);
+  // const calculateTimeLeft = (targetHour: number) => {
+  //   const now = new Date();
+  //   const target = new Date(now);
+  //   target.setUTCHours(targetHour, 0, 0, 0);
 
-    if (now.getUTCHours() >= targetHour) {
-      target.setUTCDate(target.getUTCDate() + 1);
-    }
+  //   if (now.getUTCHours() >= targetHour) {
+  //     target.setUTCDate(target.getUTCDate() + 1);
+  //   }
 
-    const diff = target.getTime() - now.getTime();
-    const hours = Math.floor(diff / (1000 * 60 * 60));
-    const minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
+  //   const diff = target.getTime() - now.getTime();
+  //   const hours = Math.floor(diff / (1000 * 60 * 60));
+  //   const minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
 
-    const paddedHours = hours.toString().padStart(2, '0');
-    const paddedMinutes = minutes.toString().padStart(2, '0');
+  //   const paddedHours = hours.toString().padStart(2, '0');
+  //   const paddedMinutes = minutes.toString().padStart(2, '0');
 
-    return `${paddedHours}:${paddedMinutes}`;
-  };
+  //   return `${paddedHours}:${paddedMinutes}`;
+  // };
 
   useEffect(() => {
     const updateCountdowns = () => {
