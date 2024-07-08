de33800924bffb34741685a86b5f86ce4a1a9b62
Amit Chaudhary <amit@ith.tech>
2024-07-08 11:52:16 +0530

    removed unused code

--- src/App.tsx
+++ src/App.tsx
@@ -67,9 +67,9 @@ const App: React.FC = () => {
 
   useEffect(() => {
     const updateCountdowns = () => {
-      setDailyRewardTimeLeft(calculateTimeLeft(0));
-      setDailyCipherTimeLeft(calculateTimeLeft(19));
-      setDailyComboTimeLeft(calculateTimeLeft(12));
+      // setDailyRewardTimeLeft(calculateTimeLeft(0));
+      // setDailyCipherTimeLeft(calculateTimeLeft(19));
+      // setDailyComboTimeLeft(calculateTimeLeft(12));
     };
 
     updateCountdowns();
